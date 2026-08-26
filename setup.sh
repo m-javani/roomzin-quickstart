573eaedb51c93031b2c4f834109ef28bb2f5312a
Mehdi Javani <mehdy.javany@gmail.com>
2026-08-26 18:13:40 +0330

    fix: download links

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -L https://github.com/m-javani/roomzin-quickstart/releases/latest/download/roomzin-quickstart.tar.gz | tar -xz
+curl -L https://github.com/m-javani/roomzin-quickstart/releases/download/latest/roomzin-quickstart.tar.gz | tar -xz
 cd roomzin-quickstart
 echo "✅ Ready. cd roomzin-quickstart. Read the quick-start.txt. "
