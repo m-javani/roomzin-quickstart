3f51aa3b95be273db66af341da2aeaad770d662b
Mehdi Javani <mehdy.javany@gmail.com>
2026-08-26 18:28:40 +0330

    fix: setup and githubflow

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 curl -L https://github.com/m-javani/roomzin-quickstart/releases/download/latest/roomzin-quickstart.tar.gz | tar -xz
+
 cd roomzin-quickstart
+
+echo "📥 Downloading latest binaries..."
+make download
+
+echo "🏗️  Building Docker images..."
+make build-images
+
 echo "✅ Ready. cd roomzin-quickstart. Read the quick-start.txt. "
